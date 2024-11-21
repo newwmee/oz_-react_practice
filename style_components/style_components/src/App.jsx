7beda620b7932d8e0921dce47fcce960ef6912e3
newwmee <dmstjs365@gmail.com>
2024-11-21 11:49:39 +0900

    styledcomponents

---
+++ style_components/style_components/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import styled from "styled-components";
+
+const BlueButton = styled.button`
+  background-color: blue;
+  color: white;
+  padding: 10px;
+  border-radius: 4px;
+  margin: 10px;
+  width: 200px;
+`;
+//스타일컴포넌트로 작성한 css코드는 jsx파일 내부에 작성 가능 
+//프로젝트 안에-css 파일이 따로 없어도 됨
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>hello</div>
+      <BlueButton>파란색버튼</BlueButton>
+    </>
+  );
+}
+
+export default App;
