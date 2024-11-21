148e8f5ccfb8cdd6cef2136663471fda915d61c9
newwmee <dmstjs365@gmail.com>
2024-11-21 21:18:16 +0900

    styled-component에서 props활용해서  스타일 적용하는 방법

--- style_components/style_components/src/App.jsx
+++ style_components/style_components/src/App.jsx
@@ -25,11 +25,16 @@ const BigBlueButton = styled(BlueButton)`
 //styled-components에서 알아서 겹치지 않는 독측한 값의 클래스 이름을 지어주고
 //그 클래스 명을 이용하여 작성한 CSS코드와 요소를 매칭 시켜 스타일 적용
 //재사용 컴포넌트는 부모컴포넌트의 클래스를 가져옴 즉
-//부모 컴포넌트의 속성을 가져다 사용할 수 있게 됨
+//부모 컴포넌트의 속성을 가져다 사용할 수 있게 됨 -> styled-components를 재사용해서 만든다는 것은
+//재사용한 컴포넌트의 클래스명을 가져다 쓰겠다
 
 const BigTextBigBlueButton = styled(BigBlueButton)`
-  font-size: 40px;
+  font-size: 30px;
   font-weight: 900;
+`;
+
+const PropsButton = styled.button`
+  background-color: ${(props) => props.backgroundcolor || "white"};
 `;
 
 //컴포넌트코드 안에서 스타일속성을 관리할 수 있게 되면 특정 컴포넌트의
@@ -49,7 +54,8 @@ function App() {
       <div>hello</div>
       <BlueButton>파란색버튼</BlueButton>
       <BigBlueButton>커다란 파란색 버튼</BigBlueButton>
-      <BigTextBigBlueButton>글자도 커다란 파란색 버튼 </BigTextBigBlueButton>
+      {/*<BigTextBigBlueButton>글자도 커다란 파란색 버튼 </BigTextBigBlueButton>*/}
+      <PropsButton>Props 버튼 </PropsButton>
     </Container>
   );
 }
