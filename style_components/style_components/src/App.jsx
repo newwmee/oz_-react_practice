23ed2f4d3bfe07f6dbedda544bb07c78a98f2a4a
newwmee <dmstjs365@gmail.com>
2024-11-21 18:08:41 +0900

    오류 확인 후 const Container 부분 주석처리 해제

--- style_components/style_components/src/App.jsx
+++ style_components/style_components/src/App.jsx
@@ -4,6 +4,15 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import styled from "styled-components";
 
+const BlueButton = styled.button`
+  background-color: blue;
+  color: white;
+  padding: 10px;
+  border-radius: 4px;
+  margin: 10px;
+  width: 200px;
+`;
+
 //스타일컴포넌트로 작성한 css코드는 jsx파일 내부에 작성 가능
 //프로젝트 안에-css 파일이 따로 없어도 됨
 
